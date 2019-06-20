bc6d0095148c38e2fc4616fa1516194a364f45c3
moonshay76 <moonshay76@gmail.com>
2019-06-20 11:28:34 -0400

    first commit

---
+++ A1dot2/src/A1dot2.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author JamiePascual
+ *
+ */
+public class A1dot2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+
+	}
+
+}
