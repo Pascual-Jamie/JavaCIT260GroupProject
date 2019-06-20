c91814aab92b775010b94748d17d92fe4d62794d
moonshay76 <moonshay76@gmail.com>
2019-06-20 11:27:26 -0400

    1st Commit

---
+++ A1dot1/src/A1dot1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author JamiePascual
+ *
+ */
+public class A1dot1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+
+	}
+
+}
